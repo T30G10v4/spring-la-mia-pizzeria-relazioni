996cb066c9a83e93c3ded865b55d89bed2b73869
T30G10v4 <matteo.giovagnotti@gmail.com>
2023-04-05 14:21:39 +0200

    First COmmit

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO `pizzas` (`id`, `description`, `name`, `price`) VALUES (NULL, 'Pomodoro, Mozzarella, Tonno, Cipolle', 'Tonno e Cipolle', '8.00')
+INSERT INTO `pizzas` (`id`, `description`, `name`, `price`) VALUES (NULL, 'Pomodoro, Mozzarella, Acciughe, Olive', 'Acciughe e Olive', '8.99')
+INSERT INTO `pizzas` (`id`, `description`, `name`, `price`) VALUES (NULL, 'Pomodoro, Mozzarella, Prosciutto Cotto, Olive, Carciofi, Capperi, Funghi, Acciughe', 'Capricciosa', '8.99')
